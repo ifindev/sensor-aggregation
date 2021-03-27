e94dd20c6ff40362884a2d2a445bd7691460adc0
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-03-28 00:35:28 +0700

    Refactor aggregation process into separate function for reusability

--- app.js
+++ app.js
@@ -20,62 +20,11 @@ app.get("/room/:area", (req, res) => {
     } else {
       roomArea = sensorData.filter((data) => data.roomArea === "roomArea3");
     }
-    console.log(roomAreaNumber);
-    /* find maximum temperature and humidity */
-    let maximumTemp = findMaximumValue(roomArea, "temperature");
-    let maximumHumd = findMaximumValue(roomArea, "humidity");
 
-    /* find minimum temperature and humidity */
-    let minimumTemp = findMinimumValue(roomArea, "temperature");
-    let minimumHumd = findMinimumValue(roomArea, "humidity");
+    /* get aggregated sensor data */
+    let respJSON = getSensorAggregate(roomArea);
 
-    /* Find total temperature & humidity for average */
-    let totalTemp = 0;
-    let totalHumd = 0;
-    roomArea.map((data) => {
-      totalTemp += data.temperature;
-      totalHumd += data.humidity;
-    });
-
-    /* Find average value */
-    let avgTemp = totalTemp / roomArea.length;
-    let avgHumd = totalHumd / roomArea.length;
-
-    /* Find median temperature & humidity */
-    let medTemp = 0;
-    let medHumd = 0;
-    let dataLen = roomArea.length;
-    let dataLenHalf = parseInt(dataLen / 2);
-
-    if (dataLen % 2 === 0) {
-      /* zero index array: median even data = data[n/2] +  data[n/2 -1] / 2*/
-      medTemp =
-        (roomArea[dataLenHalf].temperature +
-          roomArea[dataLenHalf - 1].temperature) /
-        2;
-      medHumd =
-        (roomArea[dataLenHalf].humidity + roomArea[dataLenHalf - 1].humidity) /
-        2;
-    } else {
-      medTemp = roomArea[dataLenHalf - 1].temperature;
-      medHumd = roomArea[dataLenHalf - 1].humidity;
-    }
-
-    let respJSON = {
-      temperature: {
-        min: minimumTemp,
-        max: maximumTemp,
-        median: medTemp,
-        avg: avgTemp,
-      },
-      humidity: {
-        min: minimumHumd,
-        max: maximumHumd,
-        median: medHumd,
-        avg: avgHumd,
-      },
-    };
-
+    /* Send response */
     res.send(respJSON);
   } else {
     res.send({ error: true, msg: "room is not available" });
@@ -87,6 +36,65 @@ app.get("/room/:area/day", (req, res) => {
 
   let sensorData = getDataFromJSON("data/sensor_data.json").array;
 });
+
+/* Get Aggregated data from json */
+const getSensorAggregate = (arrData) => {
+  /* find maximum temperature and humidity */
+  let maximumTemp = findMaximumValue(arrData, "temperature");
+  let maximumHumd = findMaximumValue(arrData, "humidity");
+
+  /* find minimum temperature and humidity */
+  let minimumTemp = findMinimumValue(arrData, "temperature");
+  let minimumHumd = findMinimumValue(arrData, "humidity");
+
+  /* Find total temperature & humidity for average */
+  let totalTemp = 0;
+  let totalHumd = 0;
+  arrData.map((data) => {
+    totalTemp += data.temperature;
+    totalHumd += data.humidity;
+  });
+
+  /* Find average value */
+  let avgTemp = totalTemp / arrData.length;
+  let avgHumd = totalHumd / arrData.length;
+
+  /* Find median temperature & humidity */
+  let medTemp = 0;
+  let medHumd = 0;
+  let dataLen = arrData.length;
+  let dataLenHalf = parseInt(dataLen / 2);
+
+  if (dataLen % 2 === 0) {
+    /* zero index array: median even data = data[n/2] +  data[n/2 -1] / 2*/
+    medTemp =
+      (arrData[dataLenHalf].temperature +
+        arrData[dataLenHalf - 1].temperature) /
+      2;
+    medHumd =
+      (arrData[dataLenHalf].humidity + arrData[dataLenHalf - 1].humidity) / 2;
+  } else {
+    medTemp = arrData[dataLenHalf - 1].temperature;
+    medHumd = arrData[dataLenHalf - 1].humidity;
+  }
+
+  let resultJSON = {
+    temperature: {
+      min: minimumTemp,
+      max: maximumTemp,
+      median: medTemp,
+      avg: avgTemp,
+    },
+    humidity: {
+      min: minimumHumd,
+      max: maximumHumd,
+      median: medHumd,
+      avg: avgHumd,
+    },
+  };
+
+  return resultJSON;
+};
 
 /* Find maximum data from array of json */
 const findMaximumValue = (arr, dataParam) => {
